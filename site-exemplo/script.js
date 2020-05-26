858f8e85b17f4356121f281f30546717fd3ae06e
Thiago Farias Mattos <thiago.lioncourtx@gmail.com>
2020-05-26 14:31:00 -0300

    Jogo Chromo Genius
    Testando minhas habilidades em HTML5, CSS3 e JavaScript para criar um jogo de momoria atraves de combinacao de cores.

--- site-exemplo/script.js
+++ site-exemplo/script.js
@@ -1,11 +1,29 @@
-var text = document.getElementById("num")
+chromo = [ ]
+player = [ ] ]
 
-function verificar(){
-   var n = Number(text.value)
-    alert(n)
-    if(n % 2 == 0){
-        alert("Esse número é par!")
-    } else{
-        alert("Esse número é ímpar!")
-    }
+var ch = Math.floor((Math.random() * 4)+1) * 1       
+chromo.push(ch)
+console.log(`chromo: ${ch}`)
+this.c1, this.c2, this.c3, this.c4 = 0
+
+function clicou1(c1){    
+    this.c1 = 1
+    player.push(this.c1)
+    console.log(`player: ${player}`)
+    
+}
+function clicou2(c2){    
+    this.c2 = 2
+    player.push(this.c2)
+    console.log(`player: ${player}`)
+}
+function clicou3(c3){    
+    this.c3 = 3
+    player.push(this.c3)
+    console.log(`player: ${player}`)
+}
+function clicou4(c4){    
+    this.c4 = 4
+    player.push(this.c4)
+    console.log(`player: ${player}`)
 }
